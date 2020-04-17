fdf0b7adcc97e7c1925c38166858c6d3ccb8e26b
WakkoStar <n3r0.official@gmail.com>
2020-04-17 17:33:49 +0200

    Update Main.js

--- src/Site/Main.js
+++ src/Site/Main.js
@@ -12,28 +12,30 @@ const Main = (props) => {
   const categories = props.cat
 
   return (
-      {
-        categories.map((categorie) => {
-          let path = "/" + categorie.nom
-          return(
-            <Route exact path={path} key={categorie._id}>
-                <Gallery categorie={categorie}/>
-            </Route>
-          )
-        })
-      }
-      <Route exact path="/">
-        <Rubriques cat={categories}/>
-      </Route>
-      <Route exact path="/tarifs">
-        <Tarifs />
-      </Route>
-      <Route exact path="/a-propos">
-        <Apropos />
-      </Route>
-      <Route exact path="/contact">
-        <Contact />
-      </Route>
+     <Switch>
+          {
+            categories.map((categorie) => {
+              let path = "/" + categorie.nom
+              return(
+                <Route exact path={path} key={categorie._id}>
+                    <Gallery categorie={categorie}/>
+                </Route>
+              )
+            })
+          }
+          <Route exact path="/">
+            <Rubriques cat={categories}/>
+          </Route>
+          <Route exact path="/tarifs">
+            <Tarifs />
+          </Route>
+          <Route exact path="/a-propos">
+            <Apropos />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+     </Switch>
   )
 }
 
