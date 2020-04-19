631f063def31fef6b65aeb4a9c3a533a44c0adf0
WakkoStar <n3r0.official@gmail.com>
2020-04-19 18:23:08 +0200

    public

---
+++ public/precache-manifest.131c59374b52fd9fbebdd52ba10d01e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d65a923cd538c280fa5d8499940e70a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a73e5b83ddf54a988c86",
+    "url": "/static/css/2.3f052e14.chunk.css"
+  },
+  {
+    "revision": "a4993e27889a9af8ea9a",
+    "url": "/static/css/main.71c9fd0e.chunk.css"
+  },
+  {
+    "revision": "a73e5b83ddf54a988c86",
+    "url": "/static/js/2.7bf59ee9.chunk.js"
+  },
+  {
+    "revision": "a06ac0d2397fefbcc732679e5616e766",
+    "url": "/static/js/2.7bf59ee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4993e27889a9af8ea9a",
+    "url": "/static/js/main.6abe95ff.chunk.js"
+  },
+  {
+    "revision": "3cd104e13cdf1d855a5b",
+    "url": "/static/js/runtime-main.15e5519f.js"
+  }
+]);
