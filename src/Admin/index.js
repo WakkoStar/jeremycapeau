d565424ea54968b164c0f4f75924cf87f35b4c86
WakkoStar <n3r0.official@gmail.com>
2020-04-19 23:43:50 +0200

    app

--- src/Admin/index.js
+++ src/Admin/index.js
@@ -39,46 +39,46 @@ export function Dashboard() {
 
       return (
           <Router>
-          <Switch>
-            <div className="dashboard">
-                <div id="nav-dashboard">
-                  <ul>
-                    <li><a href="http://jeremycapeau.fr">Le site</a></li>
-                    <li><Link to={`${url}/categories`}>Categories</Link></li>
-                    <li><Link to={`${url}/images`}>Images</Link></li>
-                    <li><Link to={`${url}/contact`}>Contact</Link></li>
-                    <li><Link to={`${url}/fichiers`}>Fichiers</Link></li>
-                    <li><Link to={`${url}/apropos`}>A propos</Link></li>
-                    <li><a href="https://bulkresizephotos.com/fr" target="_blank">Compression</a></li>
-                    <li>
-                      <button onClick={disconnect}>
-                          DISCONNECT
-                      </button>
-                    </li>
-                  </ul>
-                </div>
-                <div id="content-dashboard">
-                  <Route exact path={`${path}/categories`}>
-                    <Category />
-                  </Route>
-                  <Route exact path={`${path}/images`}>
-                    <Images />
-                  </Route>
-                  <Route exact path={`${path}/contact`}>
-                    <Contact />
-                  </Route>
-                  <Route exact path={`${path}/fichiers`}>
-                    <Pdfs/>
-                  </Route>
-                  <Route exact path={`${path}/apropos`}>
-                    <Apropos/>
-                  </Route>
-                  <Route exact path={`${path}/rubrique/:id`}>
-                    <Rubrique/>
-                  </Route>
-                </div>
-            </div>
-          </Switch>
+            <Switch>
+              <div className="dashboard">
+                  <div id="nav-dashboard">
+                    <ul>
+                      <li><a href="http://jeremycapeau.fr">Le site</a></li>
+                      <li><Link to={`${url}/categories`}>Categories</Link></li>
+                      <li><Link to={`${url}/images`}>Images</Link></li>
+                      <li><Link to={`${url}/contact`}>Contact</Link></li>
+                      <li><Link to={`${url}/fichiers`}>Fichiers</Link></li>
+                      <li><Link to={`${url}/apropos`}>A propos</Link></li>
+                      <li><a href="https://bulkresizephotos.com/fr" target="_blank">Compression</a></li>
+                      <li>
+                        <button onClick={disconnect}>
+                            DISCONNECT
+                        </button>
+                      </li>
+                    </ul>
+                  </div>
+                  <div id="content-dashboard">
+                    <Route exact path={`${path}/categories`}>
+                      <Category />
+                    </Route>
+                    <Route exact path={`${path}/images`}>
+                      <Images />
+                    </Route>
+                    <Route exact path={`${path}/contact`}>
+                      <Contact />
+                    </Route>
+                    <Route exact path={`${path}/fichiers`}>
+                      <Pdfs/>
+                    </Route>
+                    <Route exact path={`${path}/apropos`}>
+                      <Apropos/>
+                    </Route>
+                    <Route exact path={`${path}/rubrique/:id`}>
+                      <Rubrique/>
+                    </Route>
+                  </div>
+              </div>
+            </Switch>
           </Router>
         );
 }
